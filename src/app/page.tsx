da0b0c916d9d4b9ae7e2b86be395cd2ff1572aec
mhfed <nmhieu04091999@gmail.com>
2024-11-23 00:15:54 +0700

    Refactor: component and package dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+import { Button } from '@components/ui/button';
 import Image from 'next/image';
 import Link from 'next/link';
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@components/ui/accordion"
 
 export default function Home() {
   return (
@@ -130,6 +138,16 @@ export default function Home() {
           </button>
         </div>
       </div>
+      <Button>Click me</Button>
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+
     </div>
   );
 }
